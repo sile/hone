1fbff56663784b5389e93d5261cfff974b7236dd
Takeru Ohta <phjgt308@gmail.com>
2020-10-26 06:50:18 +0900

    Add trial module

---
+++ src/trial.rs
@@ -0,0 +1,14 @@
+#[derive(
+    Debug, Clone, Copy, PartialEq, Eq, Hash, PartialOrd, Ord, serde::Serialize, serde::Deserialize,
+)]
+pub struct TrialId(u64);
+
+impl TrialId {
+    pub const fn new(id: u64) -> Self {
+        Self(id)
+    }
+
+    pub const fn get(self) -> u64 {
+        self.0
+    }
+}
