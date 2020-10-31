07a6f85b25b66a4e7bf444e7167209e0d0a333db
Takeru Ohta <phjgt308@gmail.com>
2020-10-31 20:43:53 +0900

    Update event.rs

--- src/event.rs
+++ src/event.rs
@@ -3,6 +3,7 @@
 pub enum Event {
     Study(StudyEvent),
     Trial(TrialEvent),
+    Run(RunEvent),
 }
 
 #[derive(Debug, Clone, serde::Serialize, serde::Deserialize)]
@@ -15,9 +16,22 @@ pub enum StudyEvent {
 #[derive(Debug, Clone, serde::Serialize, serde::Deserialize)]
 #[serde(rename_all = "snake_case")]
 pub enum TrialEvent {
-    Started { trial_id: usize, run_id: usize },
-    Resumed { trial_id: usize, run_id: usize },
-    Asked { run_id: usize },
-    Told { run_id: usize },
+    Started { trial_id: usize },
     Finished { trial_id: usize },
+}
+
+#[derive(Debug, Clone, serde::Serialize, serde::Deserialize)]
+#[serde(rename_all = "snake_case")]
+pub enum ObsEvent {
+    Started { obs_id: usize, trial_id: usize },
+    Asked { obs_id: usize },
+    Told { obs_id: usize },
+    Finished { obs_id: usize },
+}
+
+#[derive(Debug, Clone, serde::Serialize, serde::Deserialize)]
+#[serde(rename_all = "snake_case")]
+pub enum RunEvent {
+    Started { run_id: usize, obs_id: usize },
+    Finished { run_id: usize, exit_code: usize },
 }
