30a22a2a3e9219bb85e3a1dcd3567d23763f7618
Takeru Ohta <phjgt308@gmail.com>
2020-11-25 18:33:01 +0900

    Add loader module

--- src/runner.rs
+++ src/runner.rs
@@ -15,6 +15,8 @@ use std::path::PathBuf;
 use std::process::{Child, Command, ExitStatus, Stdio};
 use std::time::{Duration, Instant};
 
+mod loader;
+
 pub fn spawn(
     spec: &CommandSpec,
     observation_id: ObservationId,
@@ -122,7 +124,7 @@ impl<W: Write> StudyRunner<W> {
     }
 
     pub fn load_study<R: BufRead>(&mut self, reader: EventReader<R>) -> anyhow::Result<()> {
-        let mut loader = StudyLoader::new(self);
+        let mut loader = self::loader::StudyLoader::new(self);
         loader.load(reader)
     }
 
@@ -184,22 +186,7 @@ impl<W: Write> StudyRunner<W> {
                         .remove(&finished.observation_id)
                         .expect("bug");
                     obs.exit_status = status.code();
-                    let trial_finished = self.tuner.tell(&obs)?;
-
-                    let obs_id = obs.id;
-                    let trial_id = obs.trial_id;
-                    let elapsed = self.start_time.elapsed();
-                    self.output
-                        .write(Event::Observation(ObservationEvent::Finished {
-                            obs,
-                            elapsed,
-                        }))?;
-                    self.obs_temp_dirs.remove(&obs_id);
-                    if trial_finished {
-                        self.output
-                            .write(Event::Trial(TrialEvent::Finished { trial_id }))?;
-                        self.trial_temp_dirs.remove(&trial_id);
-                    }
+                    self.tell_finished_obs(obs, self.start_time.elapsed())?;
                     did_nothing = false;
                 } else {
                     i += 1;
@@ -209,6 +196,20 @@ impl<W: Write> StudyRunner<W> {
             if did_nothing {
                 std::thread::sleep(Duration::from_millis(1));
             }
+        }
+        Ok(())
+    }
+
+    fn tell_finished_obs(&mut self, obs: Observation, elapsed: Duration) -> anyhow::Result<()> {
+        let elapsed = self.elapsed_offset + elapsed;
+        let trial_id = obs.trial_id;
+        let trial_finished = self.tuner.tell(&obs)?;
+        self.obs_temp_dirs.remove(&obs.id);
+        self.output
+            .write(Event::observation_finished(obs, elapsed))?;
+        if trial_finished {
+            self.trial_temp_dirs.remove(&trial_id);
+            self.output.write(Event::trial_finished(trial_id))?;
         }
         Ok(())
     }
@@ -324,113 +325,6 @@ impl<W: Write> StudyRunner<W> {
             req.metric_name,
             MetricInstance::new(req.metric_type, req.metric_value),
         );
-        Ok(())
-    }
-}
-
-#[derive(Debug)]
-struct StudyLoader<'a, W> {
-    study: &'a mut StudyRunner<W>,
-    trial_id_mapping: HashMap<TrialId, TrialId>,
-    obs_id_mapping: HashMap<ObservationId, ObservationId>,
-    last_elapsed: Duration,
-}
-
-impl<'a, W: Write> StudyLoader<'a, W> {
-    fn new(study: &'a mut StudyRunner<W>) -> Self {
-        Self {
-            study,
-            trial_id_mapping: HashMap::new(),
-            obs_id_mapping: HashMap::new(),
-            last_elapsed: Duration::new(0, 0),
-        }
-    }
-
-    fn load<R: BufRead>(&mut self, mut reader: EventReader<R>) -> anyhow::Result<()> {
-        let mut skip = true;
-        while let Some(event) = reader.read()? {
-            if let Some(elapsed) = event.elapsed() {
-                self.last_elapsed = elapsed;
-            }
-
-            match event {
-                Event::Study(StudyEvent::Started) => {
-                    skip = true;
-                    self.study.elapsed_offset += self.last_elapsed;
-                    self.trial_id_mapping = HashMap::new();
-                    self.obs_id_mapping = HashMap::new();
-                    self.last_elapsed = Duration::new(0, 0);
-                }
-                Event::Study(StudyEvent::Defined { .. }) => {
-                    skip = false;
-                }
-                Event::Trial(event) if !skip => {
-                    self.handle_trial_event(event)?;
-                }
-                Event::Observation(event) if !skip => {
-                    self.handle_observation_event(event)?;
-                }
-                _ => {}
-            }
-        }
-        self.study.elapsed_offset += self.last_elapsed;
-
-        Ok(())
-    }
-
-    fn handle_trial_event(&mut self, event: TrialEvent) -> anyhow::Result<()> {
-        if let TrialEvent::Started {
-            trial_id: orig_trial_id,
-        } = event
-        {
-            let trial_id = self.study.next_trial_id.fetch_and_increment();
-            self.trial_id_mapping.insert(orig_trial_id, trial_id);
-            self.study.output.write(Event::trial_started(trial_id))?;
-        }
-        Ok(())
-    }
-
-    fn handle_observation_event(&mut self, event: ObservationEvent) -> anyhow::Result<()> {
-        match event {
-            ObservationEvent::Started {
-                obs_id: orig_obs_id,
-                trial_id: orig_trial_id,
-                elapsed,
-            } => {
-                let obs_id = self.study.next_obs_id.fetch_and_increment();
-                let trial_id = *self
-                    .trial_id_mapping
-                    .get(&orig_trial_id)
-                    .ok_or_else(|| anyhow::anyhow!("unknown trial id {:?}", orig_trial_id))?;
-                self.obs_id_mapping.insert(orig_obs_id, obs_id);
-                self.study.output.write(Event::observation_started(
-                    obs_id,
-                    trial_id,
-                    self.study.elapsed_offset + elapsed,
-                ))?;
-            }
-            ObservationEvent::Finished { mut obs, elapsed } => {
-                let obs_id = *self
-                    .obs_id_mapping
-                    .get(&obs.id)
-                    .ok_or_else(|| anyhow::anyhow!("unknown observation id {:?}", obs.id))?;
-                let trial_id = *self
-                    .trial_id_mapping
-                    .get(&obs.trial_id)
-                    .ok_or_else(|| anyhow::anyhow!("unknown trial id {:?}", obs.trial_id))?;
-                obs.id = obs_id;
-                obs.trial_id = trial_id;
-
-                let elapsed = self.study.elapsed_offset + elapsed;
-                let trial_finished = self.study.tuner.tell(&obs)?;
-                self.study
-                    .output
-                    .write(Event::observation_finished(obs, elapsed))?;
-                if trial_finished {
-                    self.study.output.write(Event::trial_finished(trial_id))?;
-                }
-            }
-        }
         Ok(())
     }
 }
---
+++ src/runner/loader.rs
@@ -0,0 +1,105 @@
+use super::StudyRunner;
+use crate::event::{Event, EventReader, ObservationEvent, StudyEvent, TrialEvent};
+use crate::trial::{ObservationId, TrialId};
+use std::collections::HashMap;
+use std::io::{BufRead, Write};
+use std::time::Duration;
+
+#[derive(Debug)]
+pub struct StudyLoader<'a, W> {
+    study: &'a mut StudyRunner<W>,
+    trial_id_mapping: HashMap<TrialId, TrialId>,
+    obs_id_mapping: HashMap<ObservationId, ObservationId>,
+    last_elapsed: Duration,
+}
+
+impl<'a, W: Write> StudyLoader<'a, W> {
+    pub fn new(study: &'a mut StudyRunner<W>) -> Self {
+        Self {
+            study,
+            trial_id_mapping: HashMap::new(),
+            obs_id_mapping: HashMap::new(),
+            last_elapsed: Duration::new(0, 0),
+        }
+    }
+
+    pub fn load<R: BufRead>(&mut self, mut reader: EventReader<R>) -> anyhow::Result<()> {
+        let mut skip = true;
+        while let Some(event) = reader.read()? {
+            if let Some(elapsed) = event.elapsed() {
+                self.last_elapsed = elapsed;
+            }
+
+            match event {
+                Event::Study(StudyEvent::Started) => {
+                    skip = true;
+                    self.study.elapsed_offset += self.last_elapsed;
+                    self.trial_id_mapping = HashMap::new();
+                    self.obs_id_mapping = HashMap::new();
+                    self.last_elapsed = Duration::new(0, 0);
+                }
+                Event::Study(StudyEvent::Defined { .. }) => {
+                    skip = false;
+                }
+                Event::Trial(event) if !skip => {
+                    self.handle_trial_event(event)?;
+                }
+                Event::Observation(event) if !skip => {
+                    self.handle_observation_event(event)?;
+                }
+                _ => {}
+            }
+        }
+        self.study.elapsed_offset += self.last_elapsed;
+
+        Ok(())
+    }
+
+    fn handle_trial_event(&mut self, event: TrialEvent) -> anyhow::Result<()> {
+        if let TrialEvent::Started {
+            trial_id: orig_trial_id,
+        } = event
+        {
+            let trial_id = self.study.next_trial_id.fetch_and_increment();
+            self.trial_id_mapping.insert(orig_trial_id, trial_id);
+            self.study.output.write(Event::trial_started(trial_id))?;
+        }
+        Ok(())
+    }
+
+    fn handle_observation_event(&mut self, event: ObservationEvent) -> anyhow::Result<()> {
+        match event {
+            ObservationEvent::Started {
+                obs_id: orig_obs_id,
+                trial_id: orig_trial_id,
+                elapsed,
+            } => {
+                let obs_id = self.study.next_obs_id.fetch_and_increment();
+                let trial_id = *self
+                    .trial_id_mapping
+                    .get(&orig_trial_id)
+                    .ok_or_else(|| anyhow::anyhow!("unknown trial id {:?}", orig_trial_id))?;
+                self.obs_id_mapping.insert(orig_obs_id, obs_id);
+                self.study.output.write(Event::observation_started(
+                    obs_id,
+                    trial_id,
+                    self.study.elapsed_offset + elapsed,
+                ))?;
+            }
+            ObservationEvent::Finished { mut obs, elapsed } => {
+                let obs_id = *self
+                    .obs_id_mapping
+                    .get(&obs.id)
+                    .ok_or_else(|| anyhow::anyhow!("unknown observation id {:?}", obs.id))?;
+                let trial_id = *self
+                    .trial_id_mapping
+                    .get(&obs.trial_id)
+                    .ok_or_else(|| anyhow::anyhow!("unknown trial id {:?}", obs.trial_id))?;
+                obs.id = obs_id;
+                obs.trial_id = trial_id;
+                self.study.tell_finished_obs(obs, elapsed)?;
+            }
+        }
+        Ok(())
+    }
+}
